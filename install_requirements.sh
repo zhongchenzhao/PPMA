2e67df3da55a8936a7b1e5d72fec4ea76054f523
ZhongchenZhao <47920208+zhongchenzhao@users.noreply.github.com>
2025-06-24 15:28:22 +0800

    Add files via upload

--- install_requirements.sh
+++ install_requirements.sh
@@ -9,8 +9,8 @@ pip install -r requirements.txt
 
 
 # install mmcv
-pip install openmim;
-mim install mmcv-full;
-mim install "mmcv < 2.0.0";
+pip install openmim==0.3.9;
+mim install mmcv-full==1.7.2;
+mim install mmcv == 1.7.2;
 pip install mmsegmentation==0.30.0;
 pip install mmdet==2.28.2;
